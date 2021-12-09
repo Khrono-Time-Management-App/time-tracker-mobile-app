4ab788f974e4bbd35e0e2180421fa513359e182d
Rares Cioroga <ciorogarares@gmail.com>
2021-12-09 11:24:53 +0200

    Add Bottom tab navigation icons

---
+++ src/navigation/BottomTabBarIconOptions.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { screens } from '../constants/screens'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import PropTypes from 'prop-types';
+
+const BottomTabBarIconOptions = ({navigationRoute, isFocused, iconColor, iconSize}) => {
+  let iconName;
+
+  if (navigationRoute.name === screens.Dashboard) {
+    iconName = 'dashboard';
+  }
+
+  return <Icon name={iconName} size={iconSize} color={iconColor} />;
+};
+
+BottomTabBarIconOptions.propTypes = {
+  navigationRoute: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired,
+  isFocused: PropTypes.bool.isRequired,
+  iconColor: PropTypes.string.isRequired,
+  iconSize: PropTypes.number.isRequired,
+
+};
+export default BottomTabBarIconOptions;
--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -3,13 +3,15 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { connect } from 'react-redux';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { Colors } from '../theme/colors';
 import { screens } from '../constants/screens';
-import SignUpScreen from "../scenes/authentication/screens/SignUpScren";
+import SignUpScreen from '../scenes/authentication/screens/SignUpScren';
 import LoginScreen from '../scenes/authentication/screens/LoginScreen';
 import DashboardScreen from '../scenes/dashboard/screens/Dashboard';
 import { isAuthenticated } from '../scenes/authentication/selectors';
+import BottomTabBarIconOptions from './BottomTabBarIconOptions';
 
 const authenticationNavigatorScreenOptions = {
   headerShown: false
@@ -32,7 +34,18 @@ const MainNavigator = ({ isAuthenticated }) => {
       theme={MyTheme}
     >
       {isAuthenticated ? (
-        <BottomTabNavigator.Navigator>
+        <BottomTabNavigator.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => (
+              <BottomTabBarIconOptions
+                iconSize={size}
+                isFocused={focused}
+                navigationRoute={route}
+                iconColor={color}
+              />
+            )
+          })}
+        >
           <BottomTabNavigator.Screen name={screens.Dashboard} component={DashboardScreen}/>
         </BottomTabNavigator.Navigator>
       ) : (
